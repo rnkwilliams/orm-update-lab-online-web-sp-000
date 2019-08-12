0e935ce0db0688855d10a8f8c2b49d95ba2ead40
Ranika Williams <rnkprice07@gmail.com>
2019-08-12 23:52:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,8 @@ class Student
   end
   
   def update
-    
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
   def self.new_from_db(row)
