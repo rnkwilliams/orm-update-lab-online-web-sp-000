951bb1aa8a1930365dc29e6148013abeb1708971
Ranika Williams <rnkprice07@gmail.com>
2019-08-13 00:11:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,11 +18,10 @@ class Student
   end
   
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student(name, grade, id)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(name, grade, id)
   end
 
   def self.create_table
